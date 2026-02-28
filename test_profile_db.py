75f84324ba7f3c3394da4f97eb5d229f2da85762
AymanAbodala <ayman.abodala.2004@gmail.com>
2026-03-01 01:06:34 +0200

    change data base from loacal to cloud

--- test_profile_db.py
+++ test_profile_db.py
@@ -1,7 +1,7 @@
 from Data_base.profile_repo import save_profile, get_profile
 # removed unused import to avoid requiring pydantic in this simple test
 
-user_id = "user_001"
+user_id = "user_002"
 
 profile_data = {
     "product_category": "laptop",
